270ed1078d335121ba67f1ca33705215c1579d2d
Toshiaki Maki <tmaki@pivotal.io>
2019-12-08 22:29:48 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const element = document.createElement('h1');
+    element.innerHTML = 'Hello World!';
+    document.body.appendChild(element);
+});
